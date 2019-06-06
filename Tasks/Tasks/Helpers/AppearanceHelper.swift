92e045c8606789d3c1f56601aee4faa1b0dcb6b4
Stephanie Bowles <sbowl001@fiu.edu>
2019-06-06 11:16:37 +0800

    working on layers

---
+++ Tasks/Tasks/Helpers/AppearanceHelper.swift
@@ -0,0 +1,63 @@
+//
+//  AppearanceHelper.swift
+//  Tasks
+//
+//  Created by Stephanie Bowles on 6/6/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+import UIKit
+
+enum AppearanceHelper {
+    
+    static var titleColor = UIColor(red: 213.0/255.0, green: 185.0/255.0, blue: 164.0/255.0, alpha: 1.5)
+    
+    static func domesticMannersFont(pointSize: CGFloat) -> UIFont {
+        let font = UIFont(name: "Domestic Manners", size: pointSize)!
+        return font
+    }
+    
+    static func beyondWonderlandFont(pointSize: CGFloat) ->
+        UIFont {
+            let font = UIFont(name: "Beyond Wonderland", size: pointSize)!
+            return font
+    }
+    
+    static func setGradient(colorOne: UIColor, colorTwo: UIcolor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x:1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+       layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    static func setThemeAppearance() {
+        
+//        let textAttributes = [NSAttributedString.Key.font: domesticMannersFont(pointSize: 30)]
+//        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+         UINavigationBar.appearance().barTintColor = .black
+           let textAttributes = [NSAttributedString.Key.foregroundColor: titleColor]
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        UITextField.appearance().tintColor = .gray
+        UITextView.appearance().tintColor = .gray
+        UIBarButtonItem.appearance().tintColor = titleColor
+        UISegmentedControl.appearance().tintColor = titleColor
+        
+        
+    }
+    
+    static func style(button: UIButton) {
+        button.titleLabel?.font = domesticMannersFont(pointSize: 30)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor =  .red
+        button.layer.cornerRadius = 8
+    }
+}
+//open func setBackgroundImage(_ backgroundImage: UIImage?, for barPosition: UIBarPosition, barMetrics: UIBarMetrics)
+//open var titleTextAttributes: [NSAttributedString.Key : Any]?
+
+
+/* You may specify the font, text color, and shadow properties for the large title in the text attributes dictionary, using the keys found in NSAttributedString.h.
+ */
--- Tasks/Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks/Tasks/View Controllers/TasksTableViewController.swift
@@ -20,7 +20,10 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taskController.tasks.count
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
         
